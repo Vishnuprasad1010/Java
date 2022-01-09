71bdd182c288db5cf76ec08c73f7f7aa57c096c9
Vishnuprasad1010 <prasadvishnu743@gmail.com>
2022-01-09 14:20:12 +0530

    First code

---
+++ src/org/git/One.java
@@ -0,0 +1,13 @@
+package org.git;
+
+public class One {
+	
+	public static void main(String[] args) {
+		
+		
+		System.out.println("Start...........");
+		System.out.println("End...........");
+
+}
+
+}
